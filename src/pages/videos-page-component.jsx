c56740ded738f812102c94e493fbd798a5521d01
eelok <eelok@mail.ru>
2020-10-10 17:34:21 +0200

    style video page

--- src/pages/videos-page-component.jsx
+++ src/pages/videos-page-component.jsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, {Component} from "react";
+import './videos-page-style.scss'
 
-const VideosPage = () => {
+class VideosPage extends Component {
 
-    return(
-        <div><h1>Videos Page</h1></div>
-    )
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            playlistItems: []
+        }
+    }
+
+    async componentDidMount() {
+        console.log('did mount');
+        let key = 'AIzaSyD8_AOJJfBztUSGKL8DP2aYf4FWM_iMLx0';
+        let playlistId = 'PLQG761_nUb0p0H9d5xwdhxpsN9WMdvVzw';
+        let url = `https://www.googleapis.com/youtube/v3/playlistItems?key=${key}&playlistId=${playlistId}&part=snippet`;
+        let response = await fetch(url);
+        let youtubeObject = await response.json();
+        console.log(youtubeObject)
+        const { items } = youtubeObject;
+        console.log( items)
+        this.setState({playlistItems: items})
+    }
+
+    render() {
+        console.log('render');
+        let {playlistItems} = this.state;
+        return (
+            <div className="container">
+                {
+                    playlistItems.map(item =>
+                        <div key={item.id} className="container__content" >
+                            <img className="container__image" src={item.snippet.thumbnails.standard.url} alt=""/>
+                            <div className="container__text">
+                                <h3>{item.snippet.title}</h3>
+                                <p>{item.snippet.description}</p>
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
+        )
+    }
 }
+
 export default VideosPage;
