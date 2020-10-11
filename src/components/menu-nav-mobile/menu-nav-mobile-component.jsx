64b0699bc866856937d5fcc4766ecce456d1fd54
eelok <eelok@mail.ru>
2020-10-11 21:08:07 +0200

    fix menu items

--- src/components/menu-nav-mobile/menu-nav-mobile-component.jsx
+++ src/components/menu-nav-mobile/menu-nav-mobile-component.jsx
@@ -1,25 +1,52 @@
-import React from "react";
+import React, {Component} from "react";
 import {Link} from "react-router-dom";
 import './menu-nav-mobile.scss';
 
-const MenuNavMobile = () => {
-    return (
-        <div className="navigation">
-            <input type="checkbox" className="navigation__checkbox" id="navi-toggle"/>
-            {/*//пустой div для того, что бы там отобразить горизонтальные палки*/}
-            <label htmlFor="navi-toggle" className="navigation__button">
-                <span className="navigation__icon">&nbsp;</span>
-            </label>
-            <div className="navigation__background">&nbsp;</div>
+class MenuNavMobile extends Component {
 
-            <nav className='navigation__nav'>
-                <ul className='navigation__list'>
-                    <li><Link className="navigation__link" to={"/videos"}>Video</Link></li>
-                    <li><Link className="navigation__link" to={"/about"}>About</Link></li>
-                    <li><Link className="navigation__link" to={"/contact"}>Contact</Link></li>
-                </ul>
-            </nav>
-        </div>
-    )
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            menuOpen: false
+        };
+
+    }
+
+    handleShowMenu = () => {
+        this.setState(state => ({
+            menuOpen: !this.state.menuOpen
+        }));
+    }
+
+    handleMenuItemClicked =() =>{
+        this.setState(state => ({
+            menuOpen: false
+        }))
+    }
+
+    render() {
+        return (
+            <div className="navigation">
+                <input type="checkbox" checked={this.state.menuOpen} className="navigation__checkbox" id="navi-toggle"
+                       onChange={this.handleShowMenu}/>
+                {/*//пустой div для того, что бы там отобразить горизонтальные палки*/}
+                <label htmlFor="navi-toggle" className="navigation__button">
+                    <span className="navigation__icon">&nbsp;</span>
+                </label>
+                <div className="navigation__background">&nbsp;</div>
+
+                <nav className='navigation__nav'>
+                    <ul className='navigation__list'>
+                        <li onClick={this.handleMenuItemClicked}><Link className="navigation__link" to={"/videos"}>Video</Link></li>
+                        <li onClick={this.handleMenuItemClicked}><Link className="navigation__link" to={"/about"}>About</Link></li>
+                        <li onClick={this.handleMenuItemClicked}><Link className="navigation__link" to={"/contact"}>Contact</Link></li>
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
+
 }
+
 export default MenuNavMobile;
