64f00d4d253d042bb0d1429225ed0a216a37cbb7
eelok <eelok@mail.ru>
2020-10-11 11:07:39 +0200

    show all videos on page

--- src/pages/videos-page-component.jsx
+++ src/pages/videos-page-component.jsx
@@ -15,32 +15,31 @@ class VideosPage extends Component {
         console.log('did mount');
         let key = 'AIzaSyD8_AOJJfBztUSGKL8DP2aYf4FWM_iMLx0';
         let playlistId = 'PLQG761_nUb0p0H9d5xwdhxpsN9WMdvVzw';
-        let url = `https://www.googleapis.com/youtube/v3/playlistItems?key=${key}&playlistId=${playlistId}&part=snippet`;
+        let url = `https://www.googleapis.com/youtube/v3/playlistItems?key=${key}&playlistId=${playlistId}&part=snippet&maxResults=50`;
         let response = await fetch(url);
         let youtubeObject = await response.json();
-        console.log(youtubeObject)
         const { items } = youtubeObject;
-        console.log( items)
-        this.setState({playlistItems: items})
+        console.log(items.map(item => item.snippet));
+        this.setState({playlistItems: items});
     }
 
     render() {
         console.log('render');
         let {playlistItems} = this.state;
         return (
-            <div className="container">
+            <article className="container">
                 {
                     playlistItems.map(item =>
-                        <div key={item.id} className="container__content" >
-                            <img className="container__image" src={item.snippet.thumbnails.standard.url} alt=""/>
+                        <section key={item.id} className="container__content" >
+                            <img className="container__image" src={item.snippet.thumbnails.medium.url} alt=""/>
                             <div className="container__text">
                                 <h3>{item.snippet.title}</h3>
                                 <p>{item.snippet.description}</p>
                             </div>
-                        </div>
+                        </section>
                     )
                 }
-            </div>
+            </article>
         )
     }
 }
