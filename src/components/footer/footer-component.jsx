a429608fe9064a3802dbf120d35ef560ad9d7448
eelok <eelok@mail.ru>
2020-10-14 20:50:01 +0200

    change some items

--- src/components/footer/footer-component.jsx
+++ src/components/footer/footer-component.jsx
@@ -5,15 +5,21 @@ const Footer = () => {
     return (
         <footer className="social">
             <section className="social__icons">
-                <div className="social__item social__item--instagram">
-                    <img src={require("../../assets/instagram.svg")} alt="instagram icon"/>
-                </div>
-                <div  className="social__item social__item--vk">
-                    <img src={require("../../assets/vk.svg")} alt="vk icon"/>
-                </div>
-                <div  className="social__item social__item--youtube">
-                    <img src={require("../../assets/youtube.svg")} alt="youtube icon"/>
-                </div>
+                <a href={`https://www.instagram.com/smilebustersband/`}>
+                    <div className="social__item social__item--instagram">
+                        <img src={require("../../assets/instagram.svg")} alt="instagram icon"/>
+                    </div>
+                </a>
+                <a href={`https://vk.com/smilebusters/`}>
+                    <div className="social__item social__item--vk">
+                        <img src={require("../../assets/vk.svg")} alt="vk icon"/>
+                    </div>
+                </a>
+                <a href={`https://www.youtube.com/channel/UCXhomF9JifKzZ4CbcP1zLtA/videos`}>
+                    <div className="social__item social__item--youtube">
+                        <img src={require("../../assets/youtube.svg")} alt="youtube icon"/>
+                    </div>
+                </a>
             </section>
         </footer>
     )
