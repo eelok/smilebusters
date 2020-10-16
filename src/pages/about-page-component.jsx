5727aa87bf9067ee28748b6e06a5ff1d22bbf672
eelok <eelok@mail.ru>
2020-10-16 17:35:45 +0200

    about page wip

--- src/pages/about-page-component.jsx
+++ src/pages/about-page-component.jsx
@@ -2,7 +2,26 @@ import React from "react";
 
 const AboutPage = () => {
     return (
-        <div><h1>About Page</h1></div>
+        <article className="crew">
+            <section className='crew__story'>
+                <h3>Our Story</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aperiam cumque dolor error fuga porro
+                    quae
+                    recusandae similique ullam voluptas! Beatae doloribus dolorum explicabo maiores pariatur provident
+                    quo,
+                    soluta voluptates.</p>
+            </section>
+            <h2>Crew</h2>
+            <div className="crew__box">
+                <div className="crew__img"><img  src="https://picsum.photos/id/1027/300/200" alt="Person1"/></div>
+                <div className="crew__info">
+                    <h4 className="crew__name">Anastasia Tkachenko</h4>
+                    <h4 className="crew__instrument">Piano</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda natus
+                    nobis suscipit unde! Commodi consectetur eligendi harum officia officiis, repellat?</p>
+                </div>
+            </div>
+        </article>
     )
 }
 export default AboutPage;
