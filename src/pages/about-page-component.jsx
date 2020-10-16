82a9d0d2f524cdbd061b088278afde53983d1530
eelok <eelok@mail.ru>
2020-10-16 21:06:17 +0200

    add crew members

--- src/pages/about-page-component.jsx
+++ src/pages/about-page-component.jsx
@@ -13,12 +13,39 @@ const AboutPage = () => {
             </section>
             <h2>Crew</h2>
             <div className="crew__box">
-                <div className="crew__img"><img  src="https://picsum.photos/id/1027/300/200" alt="Person1"/></div>
+                <div className="crew__img"><img  src="https://picsum.photos/id/1027/350/200" alt="Person1"/></div>
                 <div className="crew__info">
                     <h4 className="crew__name">Anastasia Tkachenko</h4>
                     <h4 className="crew__instrument">Piano</h4>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda natus
                     nobis suscipit unde! Commodi consectetur eligendi harum officia officiis, repellat?</p>
+                </div>
+            </div>
+            <div className="crew__box">
+                <div className="crew__img"><img  src="https://picsum.photos/id/836/350/200" alt="Person1"/></div>
+                <div className="crew__info">
+                    <h4 className="crew__name">Bob Marley</h4>
+                    <h4 className="crew__instrument">Guitar</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda natus
+                        nobis suscipit unde! Commodi consectetur eligendi harum officia officiis, repellat?</p>
+                </div>
+            </div>
+            <div className="crew__box">
+                <div className="crew__img"><img  src="https://picsum.photos/id/91/350/200" alt="Person1"/></div>
+                <div className="crew__info">
+                    <h4 className="crew__name">Tom Tailor</h4>
+                    <h4 className="crew__instrument">Guitar</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda natus
+                        nobis suscipit unde! Commodi consectetur eligendi harum officia officiis, repellat?</p>
+                </div>
+            </div>
+            <div className="crew__box">
+                <div className="crew__img"><img  src="https://picsum.photos/id/856/350/200" alt="Person1"/></div>
+                <div className="crew__info">
+                    <h4 className="crew__name">Adriano Chellinatano</h4>
+                    <h4 className="crew__instrument">Drums</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda natus
+                        nobis suscipit unde! Commodi consectetur eligendi harum officia officiis, repellat?</p>
                 </div>
             </div>
         </article>
