d52f686cd17c53d87b80a6140fceed07d7709ec8
eelok <eelok@mail.ru>
2020-10-21 20:30:33 +0200

    home page wip

--- src/pages/homepage-component.jsx
+++ src/pages/homepage-component.jsx
@@ -8,10 +8,10 @@ const HomePage = () => {
         <section className="content responsive-container">
             <div className="content__text">
                 <img src="https://picsum.photos/id/1082/300/200" alt="cover"/>
-                <h1>Smilebusters</h1>
+                <h1>Smile<span className="deco-one">busters</span></h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, expedita.</p>
-                <p>jazz - funk - fusion band</p>
-                <button className='btn margin-top'>News</button>
+                <p>jazz - <span className="deco-one">funk</span> - fusion band</p>
+                <button className='btn btn-home margin-top'>News</button>
             </div>
         </section>
     )
