8e37dd0518772bef442daaa47fd58bde13c0c6b8
eelok <eelok@mail.ru>
2020-11-15 17:15:27 +0100

    change consert to afisha

--- src/components/menu-nav-mobile/menu-nav-mobile-component.jsx
+++ src/components/menu-nav-mobile/menu-nav-mobile-component.jsx
@@ -39,7 +39,7 @@ class MenuNavMobile extends Component {
                 <nav className='navigation__nav'>
                     <ul className='navigation__list'>
                         <li className='vertical-margin' onClick={this.handleMenuItemClicked}><Link className="navigation__link" to={"/videos"}>Видео</Link></li>
-                        <li className='vertical-margin' onClick={this.handleMenuItemClicked}><Link className="navigation__link" to={"/news"}>Концерты</Link></li>
+                        <li className='vertical-margin' onClick={this.handleMenuItemClicked}><Link className="navigation__link" to={"/news"}>Афиша</Link></li>
                         <li className='vertical-margin' onClick={this.handleMenuItemClicked}><Link className="navigation__link" to={"/about"}>О Нас</Link></li>
                         <li className='vertical-margin' onClick={this.handleMenuItemClicked}><Link className="navigation__link" to={"/contact"}>Контакты</Link></li>
                     </ul>
