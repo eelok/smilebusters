e0cf690e50df55253968153f03a989cad29ecbe2
eelok <eelok@mail.ru>
2020-11-15 17:12:12 +0100

    afisha wip

--- src/pages/concerts/concers-page-component.jsx
+++ src/pages/concerts/concers-page-component.jsx
@@ -3,6 +3,7 @@ import Moment from "react-moment";
 import './concerts-style.scss';
 import {mapFirebaseDoc} from "../../functions/mapFirebaseDoc";
 import {CONCERTS_URL} from "../../constants";
+import 'moment/locale/ru';
 
 class ConcertsPage extends Component {
     constructor(props) {
@@ -19,6 +20,7 @@ class ConcertsPage extends Component {
         const concerts = firestoreObject.documents
             .filter(doc => doc.fields)
             .map(mapFirebaseDoc);
+        console.log((concerts))
         this.setState({concerts: concerts});
     }
 
@@ -34,12 +36,16 @@ class ConcertsPage extends Component {
                         concerts.map((item, index) => (
                             <section className="concerts__content" key={index}>
                                 <div className='concerts__date-time'>
-                                    <Moment format="D MMMM yyyy, HH:mm">{item.startDate}</Moment>
+                                    <Moment format="DD" className="concerts__day">{item.startDate}</Moment>
+                                    <Moment format="MMMM" className="concerts__month">{item.startDate}</Moment>
+                                    <Moment format="YYYY" className="concerts__year">{item.startDate}</Moment>
+
                                 </div>
                                 <div className="concerts__information">
                                     <h3 className="concerts__title">{item.title}</h3>
                                     <p className="concerts__description">{item.description}</p>
                                     <p className="concerts__address">{item.address}</p>
+                                    <Moment format="HH:mm" className='concerts__time'>{item.startDate}</Moment>
                                 </div>
                             </section>
                         ))
