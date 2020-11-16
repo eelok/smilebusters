29475eeb71b5f62b05853ebee042064eba724329
eelok <eelok@mail.ru>
2020-11-16 16:29:02 +0100

    make concert a component

---
+++ src/components/concert/concert-item-component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Moment from "react-moment";
+
+const Concert = ({startDate, title, description, address}) =>{
+
+    return(
+        <section className="concerts__content" >
+            <div className='concerts__date-time'>
+                <Moment format="DD" className="concerts__day">{startDate}</Moment>
+                <Moment format="MMMM" className="concerts__month">{startDate}</Moment>
+                <Moment format="YYYY" className="concerts__year">{startDate}</Moment>
+
+            </div>
+            <div className="concerts__information">
+                <h3 className="concerts__title">{title}</h3>
+                <p className="concerts__description">{description}</p>
+                <p className="concerts__address">{address}</p>
+                <Moment format="HH:mm" className='concerts__time'>{startDate}</Moment>
+            </div>
+        </section>
+    );
+}
+
+export default Concert;
--- src/pages/archive-page/archive-page-component.jsx
+++ src/pages/archive-page/archive-page-component.jsx
@@ -4,6 +4,7 @@ import Moment from "react-moment";
 import {mapFirebaseDoc} from "../../functions/mapFirebaseDoc";
 import {CONCERTS_URL} from "../../constants";
 import 'moment/locale/ru';
+import Concert from "../../components/concert/concert-item-component";
 
 class Archive extends Component {
     constructor(props) {
@@ -48,20 +49,7 @@ class Archive extends Component {
                 <section className="concerts">
                     {
                         concerts.map((item, index) => (
-                            <section className="concerts__content" key={index}>
-                                <div className='concerts__date-time'>
-                                    <Moment format="DD" className="concerts__day">{item.startDate}</Moment>
-                                    <Moment format="MMMM" className="concerts__month">{item.startDate}</Moment>
-                                    <Moment format="YYYY" className="concerts__year">{item.startDate}</Moment>
-
-                                </div>
-                                <div className="concerts__information">
-                                    <h3 className="concerts__title">{item.title}</h3>
-                                    <p className="concerts__description">{item.description}</p>
-                                    <p className="concerts__address">{item.address}</p>
-                                    <Moment format="HH:mm" className='concerts__time'>{item.startDate}</Moment>
-                                </div>
-                            </section>
+                            <Concert key={index} {...item}/>
                         ))
                     }
                 </section>
