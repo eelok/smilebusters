137cb1167f920154cbef8618f7b438aadb1508f1
eelok <eelok@mail.ru>
2020-11-17 14:38:31 +0100

    make photo round

--- src/pages/about/about-page-component.jsx
+++ src/pages/about/about-page-component.jsx
@@ -17,7 +17,8 @@ class AboutPage extends React.Component {
             <article className='responsive-container'>
                 <header className='about-header'>
                     <h2 className='about-header__title'>Наша История</h2>
-                    <p className='about-header__story'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aperiam cumque dolor error fuga porro
+                    <p className='about-header__story'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
+                        aperiam cumque dolor error fuga porro
                         quae
                         recusandae similique ullam voluptas! Beatae doloribus dolorum explicabo maiores pariatur
                         provident
@@ -30,11 +31,13 @@ class AboutPage extends React.Component {
                     {
                         crew.map(person => (
                             <section className="card" key={person.id}>
-                                <img src={person.img_url} alt="Person1"/>
+                                <div className="card__image-cropper">
+                                    <img className='card__image' src={person.img_url} alt="Person1"/>
+                                </div>
                                 <div className='card__info'>
                                     <h4 className="crew__item crew__name">{person.name}</h4>
                                     <h5 className="crew__item crew__instrument">{person.instrument}</h5>
-                                    <p>{person.info}</p>
+                                    <p className="crew__item crew__description">{person.info}</p>
                                 </div>
                             </section>
                         ))
