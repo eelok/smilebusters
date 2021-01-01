e0384720e666da3536a08329b29fba697b8b6828
eelok <eelok@mail.ru>
2021-01-01 10:54:14 +0100

    change about description

--- src/pages/about/crew-data.js
+++ src/pages/about/crew-data.js
@@ -1,50 +1,44 @@
 const CREW_DATA = [
     {
-        id : 1,
-        name : "Анастасия Ткаченко",
+        id: 1,
+        name: "Анастасия Ткаченко",
         instrument: 'Клавишные',
-        img_url : "https://picsum.photos/id/836/345/200",
-        info: `Играет на фортепьяно с пяти лет. Обожает манго. Любимый предмет в школе "Мир вокруг нас".`
+        img_url: "https://picsum.photos/id/836/345/200",
+        info: `Настя играет на музыкальных инструментах с 2-лет, когда
+            папа подарил ей укулеле. В этом же возрасте она начала петь.
+            На фортепияно она играет с пяти лет. Любит манго и путешествия.`
     },
     {
-        id : 2,
-        name : "Михаил Логинов",
+        id: 2,
+        name: "Михаил Логинов",
         instrument: 'Ударные',
         img_url: "https://picsum.photos/id/1/345/200",
-        info: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-               A aperiam cumque dolor error fuga porro quae recusandae 
-               similique ullam voluptas! Beatae doloribus dolorum explicabo
-                maiores pariatur provident quo, soluta voluptates.`
+        info: `Михаил - барабанщик от бога. Его хобби - компьютерные игры и спорт`
     },
     {
-        id : 3,
-        name : "Данил Пруцкий",
+        id: 3,
+        name: "Данил Пруцкий",
         instrument: 'гитара',
-        img_url : "https://picsum.photos/id/91/345/200",
-        info: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-               A aperiam cumque dolor error fuga porro quae recusandae 
-               similique ullam voluptas! Beatae doloribus dolorum explicabo
-                maiores pariatur provident quo, soluta voluptates.`
+        img_url: "https://picsum.photos/id/91/345/200",
+        info: `Данил серъезно изучает музыкальное искусство и виртуозно владеет гитарой. Его хобби - кататься на самокате`
     },
     {
-        id : 4,
-        name : "Вячеслав Пруцкий",
+        id: 4,
+        name: "Вячеслав Пруцкий",
         instrument: 'гитара',
-        img_url : "https://picsum.photos/id/856/345/200",
-        info: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-               A aperiam cumque dolor error fuga porro quae recusandae 
-               similique ullam voluptas! Beatae doloribus dolorum explicabo
-                maiores pariatur provident quo, soluta voluptates.`
+        img_url: "https://picsum.photos/id/856/345/200",
+        info: `Вячеслав увлекается научной фантастикой. Обожает играть на бас-гитаре и плавать на длинные
+        расстояния`
     },
     {
-        id : 5,
-        name : "Алексей Ткаченко",
-        instrument: 'Менеджер',
-        img_url : "https://picsum.photos/id/1044/345/200",
-        info: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-               A aperiam cumque dolor error fuga porro quae recusandae 
-               similique ullam voluptas! Beatae doloribus dolorum explicabo
-                maiores pariatur provident quo, soluta voluptates.`
+        id: 5,
+        name: "Алексей Ткаченко",
+        instrument: 'Художественный руководитель',
+        img_url: "https://picsum.photos/id/1044/345/200",
+        info: `Алексей - движущая сила ансамбля Smilebusters. 
+            Он выбирает репертуар, делает арранжировки, организовывает репитиции и выступления.
+            Алексей любит джаз и просто хорошую музыку.
+        `
     }
 ]
 
